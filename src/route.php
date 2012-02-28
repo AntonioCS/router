d821b6659b95de31918eb906f780f797763dc1d8
AntonioCS <antoniocs@gmail.com>
2012-02-28 19:23:21 +0000

    First commit

---
+++ src/route.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class route {
+    private $_routes = array();
+    
+    private $_reroutes = array();
+    
+    public static $CONTROLLERPATH = null;
+    
+    
+    public function __construct() {
+        
+    }
+    
+    public function addRoute() {
+        
+    }
+    
+    public function addReRoute() {
+        
+    }
+    
+    public function run() {
+        
+    }
+    
+    public function setControllerPath() {
+        
+    }
+    
+    public function getControllerPath() {        
+        
+    }        
+}
